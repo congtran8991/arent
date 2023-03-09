9a968f6ee6fd350dc5ecae4481388fc21ab60808
congtran8991 <cong.tran@deliveree.com>
2023-03-09 10:24:45 +0700

    vdveg

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { routes } from "./routes";
 
 function AppRouter() {
   const showContentMenus = () => {
     const result = routes?.map((route, index) => {
-      return <Route key={index} path={route?.path} element={route.main} />;
+      return (
+        <Route
+          key={index}
+          path={route?.path}
+          element={route.element}
+        />
+      );
     });
     return <>{result}</>;
   };
-  return <><Routes>{showContentMenus()}</Routes></>;
+  return (
+    <>
+      <Routes>
+      
+        {showContentMenus()}
+        </Routes>
+    </>
+  );
 }
 
 export default AppRouter;
