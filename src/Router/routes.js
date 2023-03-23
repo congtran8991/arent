81a972500f8e13ec64585d3a8f70c7149ba9bc70
congtran8991 <cong.tran@deliveree.com>
2023-03-23 10:27:15 +0700

    vsvs

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import MyPage from '../Page/MyPage';
 import MyRecord from '../Page/MyRecord';
 import Column from '../Page/Column';
-// import {
-//     Navigate,
-//   } from "react-router-dom";
+import {
+    Navigate,
+  } from "react-router-dom";
 export const routes = [
-    // {
-    //     path: "/",
-    //     exact: false,
-    //     element: <Navigate to="/homepage"/>,
-    // },
+    {
+        path: "/",
+        exact: false,
+        element: <Navigate to="/homepage"/>,
+    },
     {
         path: "/",
         exact: false,
